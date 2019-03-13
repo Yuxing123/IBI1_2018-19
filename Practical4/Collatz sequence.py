86825653b7605feff1ccb6ad2f86e59419fe19ea
Yuxing <yuxing.18@intl.zju.edu.cn>
2019-03-13 09:42:50 +0800

    Add the pythin file

---
+++ Practical4/Collatz sequence.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 13 09:09:49 2019
+
+@author: 52935
+"""
+
+# if and for sentence
+n=45121513
+while 1==1:
+    if n%2 == 0:
+        n=n/2
+        print(n)
+    else:
+        n=3*n+1
+        print(n)
+    if n==1:
+        break
---
+++ Practical4/variables.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 13 08:58:29 2019
+
+@author: 52935
+"""
+a=532
+b=532532
+c=b/7
+d=c/11
+e=d/13
+print(a==e)
