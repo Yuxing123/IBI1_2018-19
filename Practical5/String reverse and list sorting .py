c6032d30ad5c00edf6f0845bd4a9134d088695f1
Yuxing <yuxing.18@intl.zju.edu.cn>
2019-03-20 09:27:22 +0800

    final

---
+++ Practical5/String reverse and list sorting .py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 20 09:00:59 2019
+
+@author: 52935
+"""
+
+L = "but soft what light in yonder window breaks"
+L = L[::-1]
+L = L.split(" ")
+L.sort(reverse=True)
+print(L)
