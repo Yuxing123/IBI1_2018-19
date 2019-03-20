0dea70b84a336c1b5f72a7f40ed78d1ffa4ddc59
Yuxing <yuxing.18@intl.zju.edu.cn>
2019-03-20 08:59:18 +0800

    add DNA

---
+++ Practical5/DNA.py
@@ -0,0 +1,19 @@
+DNA = "ATGCTTCAGAAAGGTCTTACG"
+DNA = list(DNA)
+myDict = {}
+for i in DNA:
+    if i in myDict:
+        myDict[i] += 1
+    else:
+        myDict[i] = 1
+print(myDict)
+
+import matplotlib.pyplot as plt
+labels='A','T','G','C'
+sizes=myDict['A'],myDict['T'],myDict['C'],myDict['G']
+colors='lightgreen','gold','lightskyblue','lightcoral'
+explode=0,0,0,0
+plt.pie(sizes,explode=explode,labels=labels,
+        colors=colors,autopct='%1.1f%%',shadow=True,startangle=50)
+plt.axis('equal')
+plt.show()
